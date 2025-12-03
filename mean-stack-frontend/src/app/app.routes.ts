b7f99287e5fc5d91e64d31699ee798207cd33ab2
daniel-nizami <dan38@njit.edu>
2025-12-02 23:31:14 -0500

    Added the signup page and able to link it from the homepage -Daniel + Dariel

--- mean-stack-frontend/src/app/app.routes.ts
+++ mean-stack-frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },   // <-- default page now
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
---
+++ mean-stack-frontend/src/app/signup/signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-signup',
+  templateUrl: './signup.html',
+  styleUrl: './signup.css'
+})
+export class SignupComponent {}
+
